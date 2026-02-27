485ddd032360587b14643875a83c94b258fc2f96
Rodolfo Jaspe <rodolfojaspe21@gmail.com>
2026-02-26 21:42:18 -0500

    Fix ESLint error: remove unused localUrl variable

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,5 @@
 // Switch between local and production backend
-const localUrl = 'http://localhost:5001'  // Uncomment for local development
 const DeployUrl = "https://toodueappbackend-production.up.railway.app"  // Uncomment for production
 
 export const useUrl = DeployUrl  // Use production backend
-// export const useUrl = localUrl  // Uncomment this line for local development
+// For local development, change to: export const useUrl = 'http://localhost:5001'
